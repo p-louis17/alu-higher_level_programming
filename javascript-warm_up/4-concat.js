c3896fccf1ae6b89277cf3abcbb804752f1c7b9b
p-louis17 <p.nsigo@alustudent.com>
2025-05-26 22:56:46 +0200

    task 4

--- javascript-warm_up/4-concat.js
+++ javascript-warm_up/4-concat.js
@@ -1,4 +1,4 @@
 #!/usr/bin/node
-const first_arg = process.argv[2];
-const second_arg = process.argv[3];
-console.log(`${first_arg} is ${second_arg}`);
+const arg1 = process.argv[2];
+const arg2 = process.argv[3];
+console.log(`${arg1} is ${arg2}`);
