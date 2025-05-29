c80fadb164c2d1f14ee0f41c85fbd1711e0cdda4
p-louis17 <p.nsigo@alustudent.com>
2025-05-29 17:41:02 +0200

    task 10

--- javascript-warm_up/10-factorial.js
+++ javascript-warm_up/10-factorial.js
@@ -1,14 +1,12 @@
 #!/usr/bin/node
 const input = process.argv[2];
-const num = Number(input);
-console.log(num)
-function factorial(n){
-  if(isNaN(n)) {
-  return 1;
+function factorial (n) {
+  if (isNaN(n)) {
+    return 1;
   }
-  if (n<=1) {
-  return 1;
+  if (n <= 1) {
+    return 1;
   }
-  return n * factorial(n-1);
+  return n * factorial(n - 1);
 }
-console.log(factorial(num));
+console.log(factorial(Number(input)));
